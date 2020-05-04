621fc5984065fbbc50d4b96ee77b501c20af2dfa
Nesiceva <masa_nesic@outlook.com>
2020-05-04 20:55:22 +0200

    Web: dodato brisanje i editovanje usluge i validacija pri editovanju

--- Aplikacija/Web/errand/src/components/UserService.vue
+++ Aplikacija/Web/errand/src/components/UserService.vue
@@ -1,19 +1,21 @@
 <template>
     <div class="userService">
         <div class="editovanje" v-if="editable">
-            <a class="link-editovanje"> <img class="razdvoji" src="../assets/remove.svg"> </a>
-            <a class="link-editovanje"> <img src="../assets/edit.svg"> </a>
+            <a class="link-editovanje" @click="showModal = true"> <img class="razdvoji" src="../assets/remove.svg"> </a>
+            <a class="link-editovanje" @click="isEditing = true"> <img src="../assets/edit.svg"> </a>
         </div>
         <div class="telo">
             <p class="ime">{{userService.service.serviceType}}</p>
             <div class="grid-div">
-                <div class="row-div">
+                <div class="row-div fix">
                     <div class="left-div"> 
                         <span v-if="isSerbian"> Maksimalno rastojanje: </span>
                         <span v-else> Maximum distance: </span>
                     </div>
                     <div class="right-div"> 
-                        {{userService.maxDist}} km
+                        <span v-if="!isEditing">{{userService.maxDist}} </span>
+                        <input v-else type="number" class="input-polje" min="0.1" max="1000" step="0.1" v-model="maxDist">
+                        km
                     </div>
                 </div>
                 <div class="row-div">
@@ -22,7 +24,17 @@
                         <span v-else> Payment type: </span>
                     </div>
                     <div class="right-div"> 
-                        {{paymentType}}
+                        <span v-if="!isEditing"> {{paymentTypeString}} </span>
+                        <select v-model="paymentType" v-else>
+                            <option v-if="isSerbian" value=0>po satu</option>
+                            <option v-if="isSerbian" value=1>po kilometru</option>
+                            <option v-if="isSerbian" value=2>fiksno</option>
+                            <option v-if="isSerbian" value=3>početna cena</option>
+                            <option v-if="!isSerbian" value=0>per hour</option>
+                            <option v-if="!isSerbian" value=1>per kilometer</option>
+                            <option v-if="!isSerbian" value=2>fixed</option>
+                            <option v-if="!isSerbian" value=3>starting price</option>
+                        </select>
                     </div>
                 </div>
                 <div class="row-div">
@@ -31,7 +43,9 @@
                         <span v-else> Payment amount: </span>
                     </div>
                     <div class="right-div"> 
-                        {{userService.paymentAmount}} rsd
+                        <span v-if="!isEditing">{{userService.paymentAmount}} </span>
+                        <input v-else type="number" class="input-polje vece" min="0" step="100" v-model="paymentAmount">
+                        rsd
                     </div>
                 </div>
                 <div class="row-div">
@@ -40,15 +54,33 @@
                         <span v-else> Minimum rating: </span>
                     </div>
                     <div class="right-div"> 
-                        {{userService.minRating}}
+                        <span v-if="!isEditing"> {{userService.minRating}} </span>
+                        <span v-else class="ocena-span"><input type="range" class="form-control-range skala" id="formControlRange" v-model="scale"> {{minRating}} </span>
+                    </div>
+                </div>
+                <div class="row-div" v-if="isEditing">
+                    <div class="left-div"> 
+                        <button type="button" class="btn btn-primary dugmeS" v-if="isSerbian" @click="saveChanges" :disabled="isInvalid">Sačuvaj</button>
+                        <button type="button" class="btn btn-primary dugmeS" v-else @click="saveChanges" :disabled="isInvalid">Save</button>
+                    </div>
+                    <div class="right-div"> 
+                        <button type="button" class="btn btn-secondary dugmeS" @click="cancel" v-if="isSerbian">Odustani</button>
+                        <button type="button" class="btn btn-secondary dugmeS" @click="cancel" v-else>Cancel</button>
                     </div>
                 </div>
             </div>
         </div>
+        <ModalAreYouSure :naslovS="'Uklanjanje usluge'" 
+                         :tekstS="'Da li ste sigurni da želite da uklonite ovu uslugu?'" 
+                         :naslovE="'Removing a service'"
+                         :tekstE="'Are you sure you want to remove this service?'"
+                         @close="showModal = false" @yes="removeService" v-if="showModal"/>
     </div>
 </template>
 
 <script>
+import {between} from "vuelidate/lib/validators"
+import ModalAreYouSure from "@/components/ModalAreYouSure"
 export default {
     props:
     {
@@ -63,13 +95,33 @@ export default {
             required:true
         }
     },
+    components:
+    {
+        ModalAreYouSure
+    },
+    data()
+    {
+        return{
+            showModal: false,
+            isEditing: false,
+            maxDist: this.userService.maxDist,
+            paymentType: this.userService.paymentType,
+            paymentAmount: this.userService.paymentAmount,
+            scale: (this.userService.minRating - 1) * 25
+        }
+    },
+    validations:
+    {
+        maxDist: { between : between(0.1,1000) },
+        paymentAmount: {between: between(0,10000000000000000000) },
+    },
     computed:
     {
         isSerbian()
         {
             return this.$store.state.isSerbian
         },
-        paymentType()
+        paymentTypeString()
         {
             if(this.isSerbian)
             {
@@ -93,8 +145,40 @@ export default {
                 else
                     return "starting"
             }
+        },
+        minRating()
+        {
+            return (Math.round((this.scale / 25 + 1) * 100) / 100).toFixed(2)
+        },
+        isInvalid()
+        {
+            return this.$v.maxDist.$invalid || this.$v.paymentAmount.$invalid
         }
-    }   
+    },
+    methods:
+    {
+        removeService()
+        {
+            console.log(this.userService)
+            this.showModal = false;
+        },
+        saveChanges()
+        {
+            this.userService.maxDist = this.maxDist
+            this.userService.minRating = this.minRating
+            this.userService.paymentAmount = this.paymentAmount
+            this.userService.paymentType = this.paymentType
+            this.isEditing = false
+        },
+        cancel()
+        {
+            this.maxDist = this.userService.maxDist
+            this.scale = (this.userService.minRating - 1) * 25
+            this.paymentAmount = this.userService.paymentAmount
+            this.paymentType = this.userService.paymentType
+            this.isEditing = false
+        }
+    }  
 }
 </script>
 
@@ -106,7 +190,7 @@ export default {
         align-items: center;
         justify-content: center;
         width: 380px;
-        height: 200px;
+        height: 215px;
         background-color: white;
         border: 1px solid grey;
         border-radius: 5px;
@@ -135,6 +219,7 @@ export default {
     .link-editovanje
     {
         height: fit-content;
+        z-index: 1;
     }
 
     .razdvoji
@@ -144,16 +229,16 @@ export default {
 
     .ime
     {
-        font-size: 20px;
+        font-size: 22px;
         font-weight: 750;
         color: rgb(48, 89, 165);
     }
 
     .grid-div
     {
-        margin-left: 20px;
+        margin-left: 15px;
         height: 150px;
-        width: 285px;
+        width: 295px;
         padding-top: 7px;
         display: flex;
         flex-direction: column;
@@ -173,7 +258,8 @@ export default {
     .left-div
     {
         text-align: center;
-        width: 170px;
+        width: 180px;
+        font-weight: 550;
     }
 
     .right-div
@@ -182,11 +268,47 @@ export default {
         width: 115px;
     }
 
+    .ocena-span
+    {
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
+    }
+
+    .dugmeS
+    {
+        height: 25px;
+        padding-left: 10px;
+        padding-right: 10px;
+        padding-bottom: 25px;
+        padding-top: 0px;
+    }
+
+    .input-polje
+    {
+        width:70px;
+        margin-right: 7px;
+        text-align: center;
+        height: 25px;
+    }
+
+    .vece
+    {
+        width:80px;
+    }
+    
+    .skala
+    {
+        width:100px;
+        margin-right: 7px;
+    }
+
     @media only screen and (max-width: 450px)
     {
         .userService
         {
             width: 110%;
+            height: 235px;
         }
 
         .grid-div
@@ -201,6 +323,12 @@ export default {
         .left-div
         {
             width: 50%;
-        }
+        }  
+
+        .skala
+        {
+            width:90px;
+            margin-right: 5px;
+        } 
     }
 </style>
