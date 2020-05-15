66ede007826c166542f6df9613a289fd0ee3529f
MashaNes <masa_nesic@outlook.com>
2020-05-16 01:08:31 +0200

    Web: dodata treca stranica pri kreiranju zahteva

--- Aplikacija/Web/errand/src/components/NewRequest3.vue
+++ Aplikacija/Web/errand/src/components/NewRequest3.vue
@@ -1,11 +1,161 @@
 <template>
     <div class="wrapper-step3">
+        <NewTask v-if="isEmpty || isAdding" :task="taskToShow" 
+                 :showCancelButton="isAdding"
+                 @sacuvaj="sacuvajTask"
+                 @odustani="odustani"/>
+        <ShowTask v-else v-for="task in myTaskList" :key="task.id" :task="task"
+                  @obrisi="obrisiTask" @izmeni="izmeniTask"/>
+        <div class="button-div-3">
+            <button type="button" class="btn btn-success" v-if="!isAdding && !isEmpty" @click="newTask">
+                <span v-if="isSerbian">Novi zadatak</span>
+                <span v-else>New task</span>
+            </button>
+        </div>
     </div>
 </template>
 
 <script>
+import NewTask from "@/components/NewTask"
+import ShowTask from "@/components/ShowTask"
 export default {
-    
+    props:
+    {
+        tasklist:
+        {
+            type: Array,
+            required: true
+        }
+    },
+    components:
+    {
+        NewTask,
+        ShowTask
+    },
+    data()
+    {
+        return{
+            myTaskList: this.tasklist,
+            isAdding: false,
+            idGenerator: -1,
+            idToShow : 0,
+            taskToShow: 
+            {
+                id: 0,
+                name: "",
+                service_type: null,
+                description: "",
+                checklist: [],
+                picture_required: false,
+                pictures: "",
+                adress: null
+            }
+        }
+    },
+    computed:
+    {
+        isSerbian()
+        {
+            return this.$store.state.isSerbian
+        },
+        isEmpty()
+        {
+            return this.myTaskList.length == 0
+        },
+        TaskToShow()
+        {
+            if(this.idToShow != 0)
+            {
+                this.myTaskList.forEach(element => {
+                    if(element.id == this.idToShow)
+                        return element
+                });
+            }
+            return {
+                id: 0,
+                name: "",
+                service_type: null,
+                description: "",
+                checklist: [],
+                picture_required: false,
+                pictures: "",
+                adress: null
+            }
+        }
+    },
+    methods:
+    {
+        odustani()
+        {
+            this.isAdding = false
+        },
+        sacuvajTask(task)
+        {
+            if(task.id == 0)
+            {
+                task.id = this.idGenerator
+                this.idGenerator = this.idGenerator - 1
+                this.myTaskList.push(task)
+            }
+            else
+            {
+                this.myTaskList.forEach((element, index) =>
+                {
+                    if(element.id == task.id)
+                        this.myTaskList.splice(index,1)
+                })
+                this.myTaskList.push(task)
+            }
+            this.isAdding = false
+            this.$emit("tasklistChanged", this.myTaskList)
+        },
+        newTask()
+        {
+            this.taskToShow = 
+                {
+                    id: 0,
+                    name: "",
+                    service_type: null,
+                    description: "",
+                    checklist: [],
+                    picture_required: false,
+                    pictures: "",
+                    adress: null
+                }
+            this.isAdding = true
+        },
+        obrisiTask(task)
+        {
+            this.myTaskList.forEach((element, index) =>
+                {
+                    if(element.id == task.id)
+                        this.myTaskList.splice(index,1)
+                })
+            this.$emit("tasklistChanged", this.myTaskList)
+            this.taskToShow = 
+                {
+                    id: 0,
+                    name: "",
+                    service_type: null,
+                    description: "",
+                    checklist: [],
+                    picture_required: false,
+                    pictures: "",
+                    adress: null
+                }
+        },
+        izmeniTask(task)
+        {
+            var resultTask = null
+            this.myTaskList.forEach(element =>
+                {
+                    if(element.id == task.id)
+                        resultTask = element
+                })
+            this.taskToShow = resultTask
+            this.isAdding = true
+        }
+    }
 }
 </script>
 
@@ -16,5 +166,15 @@ export default {
         display: flex;
         flex-direction: column;
         align-items: center;
+        margin-bottom:30px;
+    }
+
+    .button-div-3
+    {
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
+        align-items: center;
+        padding: 5px;
     }
 </style>
---
+++ Aplikacija/Web/errand/src/components/NewTask.vue
@@ -0,0 +1,338 @@
+<template>
+    <Spinner v-if="services == null" />
+    <div class="wrapper-newTask" v-else>
+        <div class="wrapper-task">
+            <div class="labelDiv">
+                <span v-if="isSerbian"> <span class="zvezdica">*</span> Naziv zadatka: </span>
+                <span v-else> <span class="zvezdica">*</span> Task name: </span>
+            </div>
+            <input type="text" v-model="myTask.name" class="name">
+            <div class="labelDiv">
+                <span v-if="isSerbian"> <span class="zvezdica">*</span> Tip usluge: </span>
+                <span v-else> <span class="zvezdica">*</span> Service type: </span>
+            </div>
+            <div class="opcije"> 
+                <v-select :options="services" label="service_type_sr" v-model="myTask.service_type" class="selekt" v-if="isSerbian" @input="serviceChanged"></v-select> 
+                <v-select :options="services" label="service_type_en" v-model="myTask.service_type" class="selekt" v-else @input="serviceChanged"></v-select> 
+                <img src="../assets/info.svg" v-if="myTask.service_type != null && isSerbian" v-b-popover.hover.bottom="myTask.service_type.description_sr">
+                <img src="../assets/info.svg" v-if="myTask.service_type != null && !isSerbian" v-b-popover.hover.bottom="myTask.service_type.description_en">
+            </div>
+            <div class="labelDiv">
+                <span v-if="isSerbian"> Opis zadatka: </span>
+                <span v-else> Task description: </span>
+            </div>
+            <textarea type="text" v-model="myTask.description" class="name" />
+            <div class="labelDiv">
+                <span v-if="isSerbian"> Spisak: </span>
+                <span v-else> Checklist: </span>
+            </div>
+            <div class="checklista">
+                <div class="spisak">
+                    <div class="element-spiska" v-for="element in myTask.checklist" :key="element.id">
+                        <div class="string-element-spiska"> {{element.item}} </div>
+                        <a @click="izbaciIzListe(element)"><img src="../assets/remove.svg" class="ikonica"></a>
+                    </div>
+                </div>
+                <div class="dodavanje-u-spisak">
+                    <input type="text" v-model="inputElement" class="input-polje" />
+                    <button type="button" class="btn btn-primary dugme-checklist" @click="dodajUChecklist">
+                        <span v-if="isSerbian">Dodaj u spisak</span>
+                        <span v-else>Add to ckecklist</span>
+                    </button>
+                </div>
+            </div>
+            <div class="row-divovi">
+                <input type="checkbox" class="picture-required" v-model="myTask.picture_required"/>
+                <div class="labelDiv">
+                    <span v-if="isSerbian"> Potrebno je dostaviti sliku </span>
+                    <span v-else> Requires a picture </span>
+                </div>
+            </div>
+            <div class="mapDiv">
+                Map goes here
+            </div>
+        </div>
+        <div class="button-div-newTask">
+             <button type="button" class="btn btn-secondary levo-dugme" v-if="showCancelButton" @click="odustani">
+                <span v-if="isSerbian">Odustani</span>
+                <span v-else>Cancel</span>
+            </button>
+            <button type="button" class="btn btn-primary" 
+                    :disabled="myTask.service_type == null || myTask.name==''"
+                    @click="sacuvaj">
+                <span v-if="isSerbian">Sačuvaj zadatak</span>
+                <span v-else>Save task</span>
+            </button>
+        </div>
+    </div>
+</template>
+
+<script>
+import vSelect from "vue-select";
+import "vue-select/dist/vue-select.css";
+import Spinner from "@/components/Spinner"
+export default {
+    props:
+    {
+        showCancelButton:
+        {
+            type: Boolean,
+            required:true
+        },
+        task:
+        {
+            type: Object,
+            required:true
+        }
+    },
+    components:
+    {
+        vSelect,
+        Spinner
+    },
+    data()
+    {
+        return{
+            myTask :
+            {
+                id: this.task.id,
+                name: this.task.name,
+                service_type: this.task.service_type,
+                description: this.task.description,
+                checklist: [],
+                picture_required: this.task.picture_required,
+                adress: this.task.address
+            },
+            oldService: this.task.service_type,
+            stringIdGenerator: -1,
+            inputElement: ""
+        }
+    },
+    computed:
+    {
+        isSerbian()
+        {
+            return this.$store.state.isSerbian
+        },
+        services()
+        {
+            return this.$store.state.allServices
+        }
+    },
+    methods:
+    {
+        serviceChanged()
+        {
+            if(this.myTask.name == "" || 
+                (this.oldService != null && 
+                (this.isSerbian && this.oldService.service_type_sr == this.myTask.name) ||
+                (!this.isSerbian && this.oldService.service_type_en == this.myTask.name)))
+            {
+                if(this.myTask.service_type == null)
+                    this.myTask.name = ""
+                else if(this.isSerbian)
+                    this.myTask.name = this.myTask.service_type.service_type_sr
+                else
+                    this.myTask.name = this.myTask.service_type.service_type_en
+            }
+            this.oldService = this.myTask.service_type
+        },
+        dodajUChecklist()
+        {
+            if(this.inputElement != "")
+            {
+                var element = {id: this.stringIdGenerator, item : this.inputElement}
+                this.myTask.checklist.push(element)
+                this.stringIdGenerator = this.stringIdGenerator - 1
+                this.inputElement = ""
+            }
+        },
+        izbaciIzListe(element)
+        {
+            this.myTask.checklist.forEach((item,index) => {
+                if(item.id == element.id)
+                    this.myTask.checklist.splice(index,1)
+            });
+        },
+        sacuvaj()
+        {
+            this.$emit("sacuvaj",this.myTask)
+        },
+        odustani()
+        {
+            this.$emit("odustani")
+        }
+    },
+    created()
+    {
+        if(this.$store.state.allServices == null)
+            this.$store.dispatch("fillServices")
+        this.task.checklist.forEach(element =>
+        {
+            this.myTask.checklist.push(element)
+        })
+    }
+}
+</script>
+
+<style scoped>
+    .wrapper-newTask, .wrapper-task
+    {
+        width:100%;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+
+    .button-div-newTask
+    {
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
+        align-items: center;
+        padding: 5px;
+        margin-top:10px;
+    }
+
+    .labelDiv
+    {
+        width:100%;
+        padding-left:10px;
+        display:flex;
+        flex-direction: row;
+        justify-content: flex-start;
+        align-items:center;
+        font-size: 18px;
+        font-weight: 600;
+    }
+
+    .name
+    {
+        width:94%;
+        margin-bottom: 10px;
+    }
+
+    .selekt
+    {
+        width: 80%;
+        margin-right: 7px;
+        background-color: white;
+        word-break:break-all;
+        background-color: rgb(236, 236, 236);
+    }
+
+    .opcije
+    {
+        display: flex;
+        flex-direction: row;
+        width: 100%;
+        justify-content: center;
+        align-items: center;
+        margin-bottom: 15px;
+        word-break:break-all;
+    }
+
+    .checklista
+    {
+        display: flex;
+        flex-direction: row;
+        width: 100%;
+        justify-content: space-around;
+        align-items: center;
+        margin-bottom: 15px;
+    }
+
+    .dodavanje-u-spisak, .spisak
+    {
+        display: flex;
+        flex-direction: column;
+        width:45%;
+    }
+
+    .dodavanje-u-spisak
+    {
+        align-items: center;
+    }
+
+    .spisak
+    {
+        border: 1px solid black;
+        min-height:150px;
+        /*word-break:break-all;*/
+    }
+
+    .element-spiska
+    {
+        width:100%;
+        border-bottom: 1px solid black;
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        justify-content: flex-end;
+        padding:7px;
+    }
+
+    .ikonica
+    {
+        width:20px;
+        height:20px;
+    }
+
+    .odlepi
+    {
+        margin-right: 7px;
+    }
+
+    .input-polje
+    {
+        width:90%;
+        word-break:break-all;
+    }
+
+    .string-element-spiska
+    {
+        width:100%;
+        text-align: left;
+    }
+
+    .dugme-checklist
+    {
+        margin-top:10px;
+    }
+
+    .zvezdica
+    {
+        color:red;
+    }
+
+    .row-divovi
+    {
+        display: flex;
+        flex-direction: row;
+        width: 100%;
+        justify-content: flex-start;
+        align-items: center;
+        margin-bottom: 15px;
+        word-break:break-all;
+    }
+
+    .picture-required
+    {
+        margin-left: 15px;
+        margin-right: 15px;
+        height:20px;
+        width:20px;
+    }
+
+    .mapDiv
+    {
+        width: 100%;
+        height:75vh;
+        border: 1px solid red;
+    }
+
+    .levo-dugme
+    {
+        margin-right:30px;
+    }
+</style>
